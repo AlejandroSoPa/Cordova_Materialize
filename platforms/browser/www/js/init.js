a8c745789a91ba397ae3eed15407980a442793f2
Alejandro <95918617+AlejandroSoPa@users.noreply.github.com>
2024-02-19 21:25:30 +0100

    finished

--- platforms/browser/www/js/init.js
+++ platforms/browser/www/js/init.js
@@ -1,6 +1,37 @@
 (function($){
   $(function(){
     $('.sidenav').sidenav();
+    $('.tabs').tabs({"swipeable":true});
+    $("#listNoticies").click(function() {
+      $.ajax({
+        method: "GET",
+        url: "https://api.spaceflightnewsapi.net/v3/articles?_limit=20",
+        dataType: "json",
+      }).done(function (msg) {
+        for(let item in msg) {
+          let elem = $("<a href='#!' class='collection-item'>"+msg[item].title+"</a>");
+          $(".collection").append(elem);
+          $(elem).click(function () {
+            $('.tabs').tabs('select','test-swipe-2');
+            console.log(msg[item]);
+            $('#test-swipe-2').empty();
+            $('#test-swipe-2').append("<h1>"+msg[item].title+"</h1><br><p>"+msg[item].summary+"</p><img src="+"'"+msg[item].imageUrl+"'"+"><br>");
+            let readMore = $("<a href='#!'>Read more...</a>");
+            $('#test-swipe-2').append(readMore);
+            $(readMore).click(function(){
+              window.open(msg[item].url,'_system'); return false;
+            });
+          });
+          $('.row').append("<div class='col s12 m6 l4'><h1>"+msg[item].title+"</h1><br><p>"+msg[item].summary+"</p><img src="+"'"+msg[item].imageUrl+"'"+"><br><a href='#!'>Read more...</a></div>");
+            let readMore2 = $("<a href='#!'>Read more...</a>");
+            $(readMore2).click(function(){
+              window.open(msg[item].url,'_system'); return false;
+            });
+        };
+      }).fail(function () {
+        alert("ERROR");
+      });
+    })
   }); // end of document ready
 })(jQuery); // end of jQuery name space
  
@@ -14,3 +45,6 @@ function onDeviceReady() {
     //document.getElementById('deviceready').classList.add('ready');
 }
 
+var options = { "swipeable": true };
+var el = document.getElementById('tabs');
+var instance = M.Tabs.init(el, options);
